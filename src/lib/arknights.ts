92b71680497c80cdb7a087be4efba3126c87f484
FabricioVera <fabriciovera7@gmail.com>
2026-03-05 11:07:41 -0300

    fix: if two races given obtain the last one

--- src/lib/arknights.ts
+++ src/lib/arknights.ts
@@ -63,25 +63,33 @@ export const fetchOperators_rhodesapi = async (): Promise<OperatorDTO[]> => {
 const removeBrackets = (text: string): string => {
   return text.replace(/[\[\]]/g, "");
 };
+
 export const fetchOperators = async (): Promise<OperatorDTO[]> => {
   try {
     const json = ArknightsOperatorsData;
 
     return json
-      .map((item: any) => ({
-        name: item.value.data.name,
-        imageURL: getWikiImageURL(item.value.data.name + "_icon"),
-        spriteURL: getWikiSpriteURL(item.value.data.name),
-        rarity: item.wiki_rarity,
-        sex: item.wiki_gender,
-        race: item.wiki_race ? removeBrackets(item.wiki_race) : "",
-        affiliation: item.wiki_faction,
-        tagList: item.value.data.tagList.filter(
-          (tag: string) => tag !== item.wiki_class && tag !== "Top Operator",
-        ),
-        class: item.wiki_class,
-        branch: item.wiki_branch,
-      }))
+      .map((item: any) => {
+        let race = item.wiki_race;
+
+        if (race && race.includes("|")) {
+          race = race.split("|")[1];
+        }
+        return {
+          name: item.value.data.name,
+          imageURL: getWikiImageURL(item.value.data.name + "_icon"),
+          spriteURL: getWikiSpriteURL(item.value.data.name),
+          rarity: item.wiki_rarity,
+          sex: item.wiki_gender,
+          race: race ? removeBrackets(race) : "",
+          affiliation: item.wiki_faction,
+          tagList: item.value.data.tagList.filter(
+            (tag: string) => tag !== item.wiki_class && tag !== "Top Operator",
+          ),
+          class: item.wiki_class,
+          branch: item.wiki_branch,
+        };
+      })
       .filter((item: any) => !item.name.includes("Reserve"));
   } catch (e) {
     const errorMsg = `Error fetching operators: ${e}`;
