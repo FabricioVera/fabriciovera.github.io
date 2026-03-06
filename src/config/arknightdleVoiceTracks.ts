e4f0f79f6c21375bb56d12fed009963d8f9ec266
FabricioVera <fabriciovera7@gmail.com>
2026-03-06 17:52:11 -0300

    feat: voicelines changes

--- src/config/arknightdleVoiceTracks.ts
+++ src/config/arknightdleVoiceTracks.ts
@@ -5,7 +5,8 @@ export interface VoiceTrackConfig {
 }
 
 export const GAME_VOICE_TRACKS: VoiceTrackConfig[] = [
-  { label: "Audio 1", num: "35", reqGuesses: 0 },
-  { label: "Audio 2", num: "21", reqGuesses: 5 },
-  { label: "Audio 3", num: "01", reqGuesses: 10 },
+  { label: "Arknights", num: "35", reqGuesses: 0 },
+  { label: "In Battle", num: "23", reqGuesses: 2 },
+  { label: "Talk", num: "01", reqGuesses: 5 },
+  { label: "Recruit", num: "11", reqGuesses: 10 },
 ];
