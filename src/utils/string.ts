e361a6a225199ab255fd5975243c96ca732a9fd0
FabricioVera <fabriciovera7@gmail.com>
2026-03-08 19:17:04 -0300

    feat: parche de letras

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,8 @@
 export function normalizeString(str: string): string {
-  return str.trim().toLowerCase();
+  return str
+    .trim()
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace("ł", "l");
 }
