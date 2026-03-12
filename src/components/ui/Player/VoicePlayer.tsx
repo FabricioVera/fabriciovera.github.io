bc291c8d141207d097e03acc5e32e4802d52b6cf
FabricioVera <fabriciovera7@gmail.com>
2026-03-12 00:58:21 -0300

    feat: troll player jiji

--- src/components/ui/Player/VoicePlayer.tsx
+++ src/components/ui/Player/VoicePlayer.tsx
@@ -1,4 +1,10 @@
-import { useState, useEffect, useRef, type ChangeEvent } from "react";
+import {
+  useState,
+  useEffect,
+  useRef,
+  type ChangeEvent,
+  type InputHTMLAttributes,
+} from "react";
 import { motion } from "framer-motion";
 import Button from "@components/ui/General/Button";
 import { logger } from "@services/logger";
@@ -7,23 +13,73 @@ import {
   type VoiceTrackConfig,
 } from "src/config/arknightdleVoiceTracks";
 import type { GameStatus } from "../../../types/game";
+import { AudioIcon, MuteIcon } from "../../Icons";
 
-/**
- * Limpia el nombre. P: name(string). R: string (limpio).
- * Efectos: Reemplaza espacios. Excepciones: Ninguna.
- */
 const cleanName = (name: string): string => {
   return name.replace(new RegExp("[ ]", "g"), "_");
 };
-
-/**
- * Genera ID de audio. P: Ninguno. R: string.
- * Efectos: Usa Math.random. Excepciones: Ninguna.
- */
 const generateAudioNumber = (): string => {
   return Math.floor(Math.random() * 39)
     .toString()
     .padStart(2, "0");
+};
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+export const InputRange = ({ className, ...props }: InputProps) => {
+  const val = Number(props.value) || 0;
+  const max = Number(props.max) || 1; // Evitamos dividir por 0 si no hay max
+  const percentage = (val / max) * 100;
+  return (
+    <input
+      type="range"
+      {...props}
+      className={`
+        appearance-none cursor-pointer outline-none 
+        [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-0 [&::-webkit-slider-thumb]:h-0
+        [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-0 [&::-moz-range-thumb]:h-0 [&::-moz-range-thumb]:border-0
+        ${className || ""}
+      `}
+      style={{
+        ...props.style,
+        background: `linear-gradient(to right, #e5e7eb ${percentage}%, #374151 ${percentage}%)`,
+      }}
+    />
+  );
+};
+
+export const AudioControl = ({
+  currentTime,
+  duration,
+  onSeek,
+}: {
+  currentTime: number;
+  duration: number;
+  onSeek: (time: number) => void;
+}) => {
+  const formatTime = (time: number) => {
+    if (time == null) return "No Time :(";
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor(time / 60);
+
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  };
+
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <span className="text-sm text-gray-400">{formatTime(currentTime)}</span>
+      <InputRange
+        type="range"
+        min={0}
+        max={duration}
+        step={0.01}
+        value={currentTime}
+        onChange={(e) => onSeek(Number(e.target.value))}
+        className="w-full h-1.5"
+      />
+    </div>
+  );
 };
 
 const playPath = "M 19 12 L 8 19 V 5 Z L 8 5";
@@ -86,12 +142,17 @@ const SingleVoicePlayer = ({
   const [audioNum, setAudioNum] = useState<string>(initialAudioNum);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [volume, setVolume] = useState<number>(0.5);
+
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [duration, setDuration] = useState<number>(0);
+
   const audioRef = useRef<HTMLAudioElement>(null);
 
   useEffect(() => {
     setAudioNum(initialAudioNum);
     setStatus("loading");
     setIsPlaying(false);
+    setCurrentTime(0);
   }, [initialAudioNum]);
 
   useEffect(() => {
@@ -118,27 +179,56 @@ const SingleVoicePlayer = ({
     setVolume(Number(e.target.value));
   };
 
+  const handleTimeUpdate = () => {
+    if (audioRef.current) {
+      setCurrentTime(audioRef.current.currentTime);
+    }
+  };
+
+  const handleLoadedMetadata = () => {
+    if (audioRef.current) {
+      setDuration(audioRef.current.duration);
+    }
+  };
+
+  const handleSeek = (time: number) => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = time;
+      setCurrentTime(time);
+    }
+  };
+
   return (
-    <div className="flex flex-row justify-center items-center gap-2 w-full">
-      <div className="w-24">
-        <AnimatedPlayButton isPlaying={isPlaying} onClick={togglePlay} />
+    <div className="flex flex-row justify-around items-center gap-2 w-full">
+      <AnimatedPlayButton isPlaying={isPlaying} onClick={togglePlay} />
+      <div className="flex flex-row w-[70%] gap-4">
+        <AudioControl
+          currentTime={currentTime}
+          duration={duration}
+          onSeek={handleSeek}
+        />
+        <div className="flex flex-row items-center gap-2 text-gray-400">
+          {volume > 0 ? <AudioIcon /> : <MuteIcon />}
+          <InputRange
+            type="range"
+            min={0}
+            max={1}
+            step={0.01}
+            value={volume}
+            onChange={handleVolumeChange}
+            className="w-4 h-20 text-accent accent-current"
+          />
+          <span className="text-sm">{Math.round(volume * 100)}%</span>
+        </div>
       </div>
-      <span className="text-sm text-gray-400">{Math.round(volume * 100)}%</span>
-      <input
-        type="range"
-        min={0}
-        max={1}
-        step={0.01}
-        value={volume}
-        onChange={handleVolumeChange}
-        className="w-full text-accent accent-current"
-      />
       <audio
         ref={audioRef}
         preload="metadata"
         onCanPlayThrough={handleCanPlay}
         onError={handleError}
         onEnded={togglePlay}
+        onTimeUpdate={handleTimeUpdate}
+        onLoadedMetadata={handleLoadedMetadata}
       />
     </div>
   );
